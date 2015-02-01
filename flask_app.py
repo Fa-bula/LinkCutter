7f777ba07fe02f48754699740d7d6ed92f631854
Fa-bula <fa_bula@mail.ru>
2015-02-01 19:11:44 +0500

    added sign_in/sign_up forms

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,21 @@
+#!/usr/bin/python
 from flask import Flask, render_template, request, redirect, url_for
 import itertools
+import hashlib
 import ast
-DIC_FILE = '/home/fabula/mysite/static/dic'
-DATA_FILE = '/home/fabula/mysite/static/cur_data'
+import os
+SCRIPT_PATH =  os.path.dirname(os.path.realpath(__file__))
+DIC_FILE = SCRIPT_PATH + '/static/dic'
+DATA_FILE = SCRIPT_PATH + '/static/cur_data'
+USERS_FILE = SCRIPT_PATH + '/static/users'
 letters = list(map(chr, range(ord('a'), ord('z')+1))) + list(map(chr, range(ord('A'), ord('Z')+1))) + list(map(chr, range(ord('0'), ord('9')+1)))
 
-def get_dict():
-    with open(DIC_FILE, 'r') as dic_file:
+def get_dict(FILE):
+    with open(FILE, 'r') as dic_file:
         return ast.literal_eval(dic_file.read())
 
-def set_dict(dic):
-    with open(DIC_FILE, 'w') as dic_file:
+def set_dict(FILE, dic):
+    with open(FILE, 'w') as dic_file:
         dic_file.write(dic.__repr__())
 
 def set_current_data(l):
@@ -38,7 +43,7 @@ def home():
 
 @app.route("/<url>/")
 def redirect_to_url(url):
-    dic = get_dict()
+    dic = get_dict(DIC_FILE)
     if (dic.get(url, 0)):
         return redirect(dic[url])
     else:
@@ -56,25 +61,47 @@ def new_link():
     else:
         cur_index += 1
     set_current_data([cur_len, cur_index])
-    dic= get_dict()
+    dic= get_dict(DIC_FILE)
     dic[short_url] = long_url
-    set_dict(dic)
+    set_dict(DIC_FILE, dic)
     return redirect(url_for('home_with_url', url=short_url))
 
 @app.route("/recent/")
 def recent():
-    dic = get_dict()
+    dic = get_dict(DIC_FILE)
     return render_template('recent_url.html', dic=dic)
 
-@app.route("/login_page/", methods=["POST"])
-def login_page():
-    return render_template('login.html')
+@app.route("/sign_in/", methods=["GET", "POST"])
+def sign_in():
+    if request.method == "POST":
+        login = request.form['login']
+        password = request.form['pass']
+        users = get_dict(USERS_FILE)
+        hash = hashlib.sha224(password).hexdigest()
+        if users.get(login, None) == hash:
+            return "OK"
+        else:
+            return "Invalid pass"
 
-@app.route("/login/", methods=["POST"])
-def login():
-    login = request.form['login']
-    password = request.form['pass']
-    return login + password
+    else:
+        return render_template('sign_in.html')
 
+@app.route('/sign_up/', methods=['GET', 'POST'])
+def sign_up():
+    users = get_dict(DIC_FILE)
+    if request.method == "POST":
+        login = request.form['login']
+        password = request.form['pass']
+        conf_pass = request.form['conf_pass']
+        if password != conf_pass:
+            return 'Passwords are different'
+        if users.get(login, None) != None:
+            return 'This login is occupied'
+        users[login] = hashlib.sha224(password).hexdigest()
+        set_dict(USERS_FILE, users)
+        return redirect(url_for('home'))
+    else:
+        return render_template('sign_up.html')
+    
 if __name__ == "__main__":
     app.run(debug=True)
